8520d2fac97f3e22df9e84d387185ac0cddacb01
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-04 14:19:57 -0800

    add new lines about numbers

--- variables.js
+++ variables.js
@@ -1,2 +1,4 @@
 Variables that are assigned to Strings:  
 - series of characters such alphabetical, numeric or symbols
+Variables that are assigned to Numbers:
+- any postive or negative number, 0, whole numbers or decimals
