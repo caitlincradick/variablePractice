3917ffa910c74baab75720370183a18ead9cd387
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-04 14:43:31 -0800

    update variables that are assigned to Booleans line

--- variables.js
+++ variables.js
@@ -2,7 +2,7 @@ Variables that are assigned to Strings:
 - series of characters such alphabetical, numeric or symbols
 Variables that are assigned to Numbers:
 - any postive or negative number, 0, whole numbers or decimals
-Variablesthat are assigned to Booleans:
+Variables that are assigned to Booleans:
 - true or false statements; for example: once the deposit has been paid the field will state true, before it's paid it will show as false. 
 
 var email = "hello@gmail.com"
