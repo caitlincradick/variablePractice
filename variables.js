7066f1f02355d7b04e699801b95b5c896d7ec4c1
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-04 14:40:29 -0800

    add variables and console log

--- variables.js
+++ variables.js
@@ -4,3 +4,15 @@ Variables that are assigned to Numbers:
 - any postive or negative number, 0, whole numbers or decimals
 Variablesthat are assigned to Booleans:
 - true or false statements; for example: once the deposit has been paid the field will state true, before it's paid it will show as false. 
+
+var email = "hello@gmail.com"
+console.log (email)
+var depositPaid ="true"
+console.log (depositPaid)
+var startingAge ="37"
+console.log (startingAge)
+
+
+
+
+
