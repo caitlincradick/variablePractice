cd3267f18fca39f5f58bd4f854cd0c2a2f801c56
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-05 13:04:41 -0800

    add new variables

--- variables.js
+++ variables.js
@@ -1,16 +1,34 @@
-Variables that are assigned to Strings:  
-- series of characters such alphabetical, numeric or symbols
-Variables that are assigned to Numbers:
-- any postive or negative number, 0, whole numbers or decimals
-Variables that are assigned to Booleans:
-- true or false statements; for example: once the deposit has been paid the field will state true, before it's paid it will show as false. 
+// Variables that are assigned to Strings:  
+// - series of characters such alphabetical, numeric or symbols
+// Variables that are assigned to Numbers:
+// - any postive or negative number, 0, whole numbers or decimals
+// Variables that are assigned to Booleans:
+// - true or false statements; for example: once the deposit has been paid the field will state true, before it's paid it will show as false. 
+
+// var email = "hello@gmail.com"
+// console.log (email)
+// var depositPaid ="true"
+// console.log (depositPaid)
+// var startingAge ="37"
+// console.log (startingAge)
+
+// "id": "email",
+//       "email": "hello@gmail.com"
+// "command": console.log (email)
+
+      // type specific configuration attribut
+
+
+
+    //   $String firstName; "Caitlin"
+    //   $console.log(firstName)
 
 var email = "hello@gmail.com"
-console.log (email)
-var depositPaid ="true"
-console.log (depositPaid)
-var startingAge ="37"
-console.log (startingAge)
+console.log(email)
+
+var email ="goodbye@gmail.com"
+console.log(email)
+
 
 
 
